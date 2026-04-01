ac65849df66858651edd8734cb9f680a08a979e7
hurakot <pavel.e.patokin@gmail.com>
2026-04-02 00:44:03 +0300

    Add files via upload

---
+++ Лаб_работа_6/app/config.py
@@ -0,0 +1,17 @@
+
+#-----------------------------------------------
+#Страница про конфигурацию всего веб-приложения.
+#-----------------------------------------------
+
+import os
+
+SECRET_KEY = 'secret-key'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///project.db'
+# SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://user:password@std-mysql.ist.mospolytech.ru/db_name'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = True
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media', 'images')
+
+#Страница про конфигурацию всего веб-приложения.
